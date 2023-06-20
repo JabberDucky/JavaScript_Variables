69857f9e63987a15956cab589836a8e98b43c79f
Katherine Vail <jabberduckygame@gmail.com>
2023-06-20 14:52:51 -0500

    completed exercise

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
-// Exercise 2
+const firstName = "Kati";
+let lastName = "Vail";
+var age = 33;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss"
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
